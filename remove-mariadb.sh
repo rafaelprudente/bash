37bcccd5ec6bc1e98b47f3fc0b6deaf5bb8f8b09
Rafael Prudente <rafael.prudente.santos@gmail.com>
2019-10-19 14:36:45 -0300

    new scripts

---
+++ remove-mariadb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get remove mariadb-server
+sudo apt-get purge mariadb-server
+sudo apt-get autoremove
+
---
+++ remove-phpadmin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get remove phpmyadmin
+sudo apt-get purge phpmyadmin
+sudo apt-get autoremove
